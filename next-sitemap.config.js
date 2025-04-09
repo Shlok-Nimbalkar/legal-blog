9c8525dbd9bf4cbf37a4cdc0877c045b365380d0
Shlok Nimbalkar <shlok.nimbalkar@icloud.com>
2025-04-09 19:08:35 +0530

    fix: update sitemap config for App Router on Vercel

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,8 @@
-const siteMetadata = require("./src/utils/siteMetaData");
-
-
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: siteMetadata.siteUrl,
-    generateRobotsTxt: true,
-  }
+  siteUrl: 'https://create-blog-with-nextjs.vercel.app', // <-- change this to your final site
+  generateRobotsTxt: true,
+  generateIndexSitemap: true,
+  sitemapSize: 5000,
+  outDir: 'public', // <-- This avoids the /out folder entirely
+};
